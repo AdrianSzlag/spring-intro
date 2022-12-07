9168f9321ad63da1abc3de08dc5a7e43cc79e605
Adrian <adrian.szlag@student.pk.edu.pl>
2022-12-07 16:56:55 +0100

    13 zad

--- src/main/java/com/example/MyNameMessageService.java
+++ src/main/java/com/example/MyNameMessageService.java
@@ -1,4 +1,9 @@
 package com.example;
 
 public class MyNameMessageService implements MessageService {
+    private final String[] messages = {"Wiadomosc 1\n", "Wiadomosc 2\n", "Wiadomosc 3\n", "Wiadomosc 4\n","Wiadomosc 5\n", "Wiadomosc 6\n", "Wiadomosc 7\n", "Wiadomosc 8\n", "Wiadomosc 9\n", "Wiadomosc 10\n"};
+    @Override
+    public String getMessage() {
+        return "Adrian Szlag";
+    }
 }
