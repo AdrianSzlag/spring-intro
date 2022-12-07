3bbc2661c7e7ab4dd1d94685476610981c28b93c
Adrian <adrian.szlag@student.pk.edu.pl>
2022-12-07 17:03:01 +0100

    15 zad

--- src/main/java/com/example/MyNameMessageService.java
+++ src/main/java/com/example/MyNameMessageService.java
@@ -1,6 +1,10 @@
 package com.example;
 
+import org.springframework.stereotype.Component;
+
+@Component("messageService")
 public class MyNameMessageService implements MessageService {
+
     private final String[] messages = {"Wiadomosc 1\n", "Wiadomosc 2\n", "Wiadomosc 3\n", "Wiadomosc 4\n","Wiadomosc 5\n", "Wiadomosc 6\n", "Wiadomosc 7\n", "Wiadomosc 8\n", "Wiadomosc 9\n", "Wiadomosc 10\n"};
     @Override
     public String getMessage() {
--- src/main/java/com/example/RandomTextMessageService.java
+++ src/main/java/com/example/RandomTextMessageService.java
@@ -1,7 +1,10 @@
 package com.example;
 
+import org.springframework.stereotype.Component;
+
 import java.util.Random;
 
+@Component("randomTextMessageService")
 public class RandomTextMessageService implements MessageService{
     private final String[] messages = {"Wiadomosc 1\n", "Wiadomosc 2\n", "Wiadomosc 3\n", "Wiadomosc 4\n", "Wiadomosc 5\n", "Wiadomosc 6\n", "Wiadomosc 7\n", "Wiadomosc 8\n", "Wiadomosc 9\n", "Wiadomosc 10\n"};
     private final Random random;
